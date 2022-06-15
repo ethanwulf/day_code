623b3c7e8aed8228b195f76fe9f54ce028edadd6
Ethan Wulf <32464090+ethanwulf@users.noreply.github.com>
2022-06-15 12:21:14 -0400

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int day_code{};
+
+    cout << "enter day code:";
+    cin >> day_code;
+
+
+    switch (day_code) {
+    case 0: cout << "Sunday";
+        break;
+    case 1: cout << "Monday";
+        break;
+    case 2: cout << "Tuesday";
+        break;
+    case 3: cout << "Wednesday";
+        break;
+    case 4: cout << "Thursday";
+        break;
+    case 5: cout << "Friday";
+        break;
+    case 6: cout << "Saturday";
+        break;
+    default: cout << "Error - illegal day code";
+    }
+}
